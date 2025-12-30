674cd9048e1f3579ed067eb51adc677a0a86db56
Afitab Iyigun <afitab@bu.edu>
2025-12-29 19:37:31 -0500

    updated mediation pipeline

--- causal_mediation/mediation.py
+++ causal_mediation/mediation.py
@@ -20,7 +20,7 @@ llm = LanguageModel("meta-llama/Meta-Llama-3.1-70B", device_map="auto")
 with open("dataset_generation/paired_dataset.json") as f:
     data = json.load(f)
 
-total = 20
+total = 1
 subset = random.sample(data, k=total)
 intervened_prob_diff = []
 
@@ -39,49 +39,48 @@ for example in subset:
         "Answer: ("
     )
 
+    source_ids = llm.tokenizer(f" {example['SourceAnswer']}")["input_ids"][1]
+    base_ids   = llm.tokenizer(f" {example['BaseAnswer']}")["input_ids"][1]
+    num_layers = len(llm.model.layers)
+    base_len = len(llm.tokenizer(base_prompt).input_ids)
+    
+    source_hs = []
+    with torch.no_grad():
+        with llm.trace(source_prompt, remote=True):
+            source_hs = [layer.output[0].save() for layer in llm.model.layers] # save the hidden states of the last layer of each layer
+            # source_output = llm.output.save() # saving the output of the model
+            
+    causal_effects = []
+    intervened_prob_diff = []
+
     num_tokens = len(llm.tokenizer(base_prompt).input_ids) # get number of tokens in prompt
 
-    with torch.no_grad():
-        with llm.trace(remote=True) as tracer:
-            with tracer.invoke(source_prompt):
-                source_hs = [layer.output[0].save() for layer in llm.model.layers] # save the hidden states of the last layer of each layer
-                source_output = llm.output.save() # saving the output of the model
+    for layer_idx in range(llm.config.num_hidden_layers): # iterating through all layers
+        causal_effect_per_layer = []
+        for token_idx in range(num_tokens): # iterating through all tokens
+            with torch.no_grad():
+                with llm.trace(base_prompt, remote=True):
+                    # changing the value of the base activation to the source value
+                    llm.model.layers[layer_idx].output[0][:, token_idx, :] = \
+                        source_hs[layer_idx][:, token_idx, :]
 
-            source_id = llm.tokenizer(f" {example['SourceAnswer']}")["input_ids"][1]
-            base_id   = llm.tokenizer(f" {example['BaseAnswer']}")["input_ids"][1]
-            
-        causal_effects = []
-        for layer_idx in range(llm.config.num_hidden_layers): # iterating through all layers
-            causal_effect_per_layer = []
-            for token_idx in range(num_tokens): # iterating through all tokens
-                    with tracer.invoke(base_prompt):
-                        # changing the value of the base activation to the source value
-                        llm.model.layers[layer_idx].output[0][:, token_idx, :] = \
-                            source_hs[layer_idx][:, token_idx, :]
+                    # getting intervened output & comparing to base output
+                    intervened_logits = llm.output.logits[:, -1, :]
+                    intervened_probs = intervened_logits.softmax(dim=-1)
 
-                        # getting intervened output & comparing to base output
-                        intervened_logits = llm.output.logits[:, -1, :]
-                        intervened_probs = intervened_logits.softmax(dim=-1)
+                    intervened_prob_diff = (intervened_probs[0, source_ids] - intervened_probs[0, base_ids]).item().save()
 
-                        intervened_prob_diff = (intervened_probs[0, source_id] - intervened_probs[0, base_id]).item().save()
-
-                    causal_effect_per_layer.append(intervened_prob_diff)
-            causal_effects.append(causal_effect_per_layer)
+                causal_effect_per_layer.append(intervened_prob_diff)
+                
+        causal_effects.append(causal_effect_per_layer)
         
-
 #%% plotting the causal effects
+token_strs = [llm.tokenizer.decode([tid]) for tid in base_ids[:num_tokens]]
+
 fig = px.imshow(
     causal_effects,
-    x=llm.tokenizer(base_prompt).input_ids,
+    x=token_strs,
     y=list(range(llm.config.num_hidden_layers)),
-    template='simple_white',
-    color_continuous_scale=[[0, '#FFFFFF'], [1, "#A59FD9"]]
+    template="simple_white",
 )
-
-fig.update_layout(
-    xaxis_title='token',
-    yaxis_title='layer',
-    yaxis=dict(autorange='min')
-)
-
-fig
+fig.write_html("causal_mediation/causal_effects.png", scale=2)
